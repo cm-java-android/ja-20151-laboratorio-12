6167fe80d236b77bfc86960bacc524b60a984ca5
cm-java-android <cm.java.android@gmail.com>
2015-08-02 20:28:49 -0300

    Laboratorio 12 implementado e testado!

--- src/br/java/android/laboratorio12/PrincipalActivity.java
+++ src/br/java/android/laboratorio12/PrincipalActivity.java
@@ -1,16 +1,115 @@
 package br.java.android.laboratorio12;
 
+import java.io.InputStream;
+import java.net.URL;
+
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class PrincipalActivity extends Activity {
+	
+	private ImageView imageView;
+	private ProgressDialog dialog;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_principal);
+		
+		imageView = (ImageView) findViewById(R.id.imageView1);
+		
+		findViewById(R.id.workerThreadButton).setOnClickListener(
+				new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View vParam) {
+				acaoNoWorkThread();
+				
+			}
+		});
+		
+		findViewById(R.id.asyncTaskButton).setOnClickListener(
+				new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View vParam) {
+				acaoNoAsyncTask();
+			}
+		});
+		
+	}
+	
+	protected void acaoNoAsyncTask() {
+
+		final String urlImagem = ((EditText)findViewById(R.id.urlEditText))
+				.getText().toString();
+		
+		// String - Parâmetro
+		// Void - Progresso
+		// Drawable - Retorno
+		new AsyncTask<String, Void, Drawable>(){
+
+			@Override
+			protected void onPreExecute() {
+				dialog = ProgressDialog.show(
+						PrincipalActivity.this, // Contexto
+						"Aguarde", // Titulo
+						"Estamos buscando a sua imagem..."); // Mensagem
+			}
+			
+			@Override
+			protected Drawable doInBackground(String... params) {
+				Drawable d = carregaImagemDaInternet(params[0]);
+				return d;
+			}
+			
+			protected void onPostExecute(Drawable result) {
+				imageView.setImageDrawable(result);
+				dialog.dismiss();
+			};		
+			
+		}.execute(new String[]{urlImagem});
+		
+	}
+
+	protected void acaoNoWorkThread() {
+		
+		final String urlImagem = ((EditText)findViewById(R.id.urlEditText))
+				.getText().toString();
+		
+		new Thread(){
+			public void run(){
+				final Drawable desenho = carregaImagemDaInternet(urlImagem);
+				
+				imageView.post(new Runnable() {
+					
+					@Override
+					public void run() {
+						imageView.setImageDrawable(desenho);
+						
+					}
+				});
+			}
+		}.start();
+		
+	}
+
+	public static Drawable carregaImagemDaInternet(String url) {
+		try {
+			InputStream is = (InputStream) new URL(url).getContent();
+			Drawable d = Drawable.createFromStream(is, "src name");
+			return d;
+		} catch (Exception e) {
+			return null;
+		}
 	}
 
 	@Override
